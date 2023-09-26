b5960764169a24f75db34562be845c0ad08909ed
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-09-26 14:12:05 +0700

    commit

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const height = parseFloat(heightInput.value) / 100; // Konversi tinggi dari cm ke m
         const bmi = weight / (height * height);
         const roundedBMI = bmi.toFixed(2);
-        let catergory = '';
+        let category = '';
 
         if (!isNaN(roundedBMI)) {
             if (roundedBMI < 18.5) {
-                catergory = "Underweight"
+                category = "Underweight"
             } else if (roundedBMI <= 24.9) {
-                catergory = "Normal"
+                category = "Normal"
             } else if (roundedBMI <= 25) {
-                catergory = "Overweight"
+                category = "Overweight"
             } else if (roundedBMI <= 30) {
-                catergory = "OLAHRAGA TOLOL"
+                category = "HARUS OLAHRAGA TOLOL"
             } else if (roundedBMI > 30) {
-                catergory = "OBESITAS BANGSAT"
+                category = "OBESITAS BANGSAT"
             }
-            resultDiv.innerText = `BMI Anda adalah: ${roundedBMI} berarti anda  ${catergory}`;
+            resultDiv.innerText = `BMI Anda adalah: ${roundedBMI} berarti anda  ${category}`;
         } else {
             resultDiv.innerText = "Mohon masukkan berat badan dan tinggi badan yang valid.";
         }
